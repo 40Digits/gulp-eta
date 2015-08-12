2329972c49aa04a51e3f568d1eb9c97c62426399
Chad Watson <chad@40digits.com>
2015-08-12 16:38:44 -0500

    Refactors the way that tasks are setup and adds "before" option to handle task dependencies

--- index.js
+++ index.js
@@ -6,40 +6,18 @@
 
 var p = require('path');
 var plugins = require('gulp-load-plugins')({ lazy: true });
-var browserSync = require('browser-sync');
-var tasks = require('./lib/tasks');
-var setupConfig = require('./lib/config/setup.js');
+var defaultConfig = require('./lib/config');
+var setupTasks = require('./lib/util/setupTasks.js');
+var config = {};
 
 module.exports = function(gulp, options) {
-  // A convenience function to return a gulp task
-  var getTask = function(task, config) {
-    return tasks[task](gulp, plugins, config);
-  };
-  var config = {};
-
-  // add browser-sync to the plugins so that it can be injected
-  // into all of the dependencies
-  plugins.browserSync = browserSync;
-  
-  // Setup the config from the user options
+  // setup the options object if one hasn't been defined
   options = options ? options : {};
+  // set the base dir of the parent app
   options.appDir = p.dirname(module.parent.filename);
-  config = setupConfig(options);
+  // get the merged config
+  config = defaultConfig(options);
 
-  // The Tasks
-  gulp.task('init', getTask('init', config));
-  gulp.task('watch', getTask('watch', config));
-  gulp.task('images', getTask('images', config));
-  gulp.task('sass', getTask('sass', config));
-  gulp.task('sprites', getTask('sprites', config));
-  gulp.task('static', getTask('static', config));
-  gulp.task('symbols', getTask('symbols', config));
-  gulp.task('browserify', getTask('browserify', config));
-  gulp.task('browserSync', ['watch'], getTask('browserSync', config));
-  gulp.task('uglifyJs', getTask('uglifyJs', config));
-  gulp.task('minifyCss', ['sass'], getTask('minifyCss', config));
-  gulp.task('production', ['minifyCss', 'uglifyJs']);
-  gulp.task('default', config.default.tasks);
-
-  return gulp;
+  // Return Gulp with our Tasks
+  return setupTasks(gulp, plugins, config);
 };
