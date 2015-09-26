c793592f78acedc8459511082b230a3b22633b9c
Chad Watson <chad@40digits.com>
2015-09-26 08:43:00 -0500

    Refactors up the default configuration, because it was just too stinking complex

--- index.js
+++ index.js
@@ -6,18 +6,19 @@
 
 var p = require('path');
 var plugins = require('gulp-load-plugins')({ lazy: true });
-var defaultConfig = require('./lib/config');
 var setupTasks = require('./lib/util/setupTasks.js');
-var config = {};
+var config = require('./lib/config.js');
 
 module.exports = function(gulp, options) {
-  // setup the options object if one hasn't been defined
-  options = options ? options : {};
-  // set the base dir of the parent app
-  options.appDir = p.dirname(module.parent.filename);
-  // get the merged config
-  config = defaultConfig(options);
+  var appDir = p.dirname(module.parent.filename);
 
-  // Return Gulp with our Tasks
-  return setupTasks(gulp, plugins, config);
+  // fallback to the app's package.json
+  // options if nothing has been passed in
+  if (!options) {
+    var package = require(p.join(appDir, 'package.json'));
+    options = package.eta ? package.eta : {};
+  }
+
+  // return gulp with our tasks
+  return setupTasks(gulp, plugins, config(options, appDir));
 };
